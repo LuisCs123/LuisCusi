bef694cc52b63df5706a266476c54cdef44730a0
Luis Cusi <54385753+iLuisCusi@users.noreply.github.com>
2020-06-07 18:25:05 -0500

    Version 2.0 Diseño
    Falta explicar gizmo para creacion de pilas con random rotation.

---
+++ Version1.0_Diseño/Assets/Scripts/RespawnCar.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+[ExecuteInEditMode]
+public class RespawnCar : MonoBehaviour
+{
+    [SerializeField]private GameObject carPrefab = null;
+    private Vector3 spawnPosition = Vector3.one;
+
+
+    void Start()
+    {
+        spawnPosition = new Vector3(-3, 10, -10);
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.black;
+        Gizmos.DrawWireSphere(spawnPosition, 1);
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            SpawnObject();
+            Debug.Log("D WAS PRESSED");
+        }
+    }
+    public void SpawnObject()
+    {
+        Instantiate(carPrefab, spawnPosition, carPrefab.transform.rotation);
+    }
+
+}
